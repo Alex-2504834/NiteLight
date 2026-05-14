c73f71d8420982ed311875dd69bac45c4bcd4a1f
Alex-2504834 <232655997+Alex-2504834@users.noreply.github.com>
2026-05-14 09:58:03 +0100

    Icons

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -2,14 +2,20 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeScreen from "../screens/HomeScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+import MapScreen from "../screens/MapScreen"
+import { createTabBarStyles } from "../theme/styles";
+import { useTheme } from "../theme/useTheme";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator()
 
 export default function TabNavigator() {
+    const { colour } = useTheme();
     return (
-        <Tab.Navigator screenOptions={{headerShown: false}}>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Settings" component={SettingsScreen}/>
+        <Tab.Navigator screenOptions={{headerShown: false, ...createTabBarStyles(colour)}}>
+            <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="home-outline" size={size} color={color}/>)}}/>
+             <Tab.Screen name="Map" component={MapScreen} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="map-outline" size={size} color={color}/>)}}/>
+            <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: ({ color, size}) => (<Ionicons name="settings-outline" size={size} color={color}/>)}}/>
         </Tab.Navigator>
     )
 }
--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -11,5 +11,24 @@ export const globalStyles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "600"
     },
+});
 
+export const createTabBarStyles = (colour: any) => ({
+    tabBarStyle: {
+        backgroundColor: colour.surface,
+        borderTopColor: colour.border,
+
+        height: 120,
+        paddingBottom: 10,
+        paddingTop: 10,
+    },
+
+    tabBarActiveTintColor: colour.primary,
+    tabBarInactiveTintColor: colour.textSecondary,
+
+    headerStyle: {
+        backgroundColor: colour.surface,
+    },
+
+    headerTintColor: colour.text,
 });

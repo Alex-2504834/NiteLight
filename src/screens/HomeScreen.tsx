d8d5568edb022aff2861a6910d09db1b2bc03734
Amcgee123 <austinmcgee33@gmail.com>
2026-05-21 14:43:12 +0100

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 import { globalStyles } from "../theme/styles";
 import { useTheme } from "../theme/useTheme";
@@ -7,8 +7,28 @@ export default function HomeScreen() {
     const { colour } = useTheme();
 
     return (
-        <View style={[globalStyles.screen, {backgroundColor: colour.background}]}>
-          <Text style={[globalStyles.title, {color: colour.text}]}>Home Screen</Text>
+        <View
+            style={[
+                globalStyles.screen,
+                { backgroundColor: colour.background }
+            ]}
+        >
+<Image
+    source={require("../../assets/logo.png")}
+    style={{
+        width: 200,
+        height: 200,
+    }}
+/>
+
+            <Text
+                style={[
+                    globalStyles.title,
+                    { color: colour.text }
+                ]}
+            >
+                Home Screen
+            </Text>
         </View>
     );
 }

f3e9f4a651fc8bd32917c06ccdb0e1e77cdd27ed
Alex=2504834 <M2504834@middlesbro.ac.uk>
2026-05-21 23:46:32 +0100

    Icons!
    I probably broke the iOS one but shhhh

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,21 +1,32 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "../screens/HomeScreen";
 import SettingsScreen from "../screens/SettingsScreen";
-import MapScreen from "../screens/MapScreen"
+import MapScreen from "../screens/MapScreen";
+
 import { createTabBarStyles } from "../theme/styles";
 import { useTheme } from "../theme/useTheme";
-import Ionicons from "react-native-vector-icons/Ionicons";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
+
+type IconProps = {
+  color: string;
+  size: number;
+};
+
+const HomeTabIcon     = ({ color, size }: IconProps) => (<Ionicons name="home-outline" size={size} color={color} />);
+const MapTabIcon      = ({ color, size }: IconProps) => (<Ionicons name="map-outline" size={size} color={color} />);
+const SettingsTabIcon = ({ color, size }: IconProps) => (<Ionicons name="settings-outline" size={size} color={color} />);
 
 export default function TabNavigator() {
-    const { colour } = useTheme();
-    return (
-        <Tab.Navigator screenOptions={{headerShown: false, ...createTabBarStyles(colour)}}>
-            <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="home-outline" size={size} color={color}/>)}}/>
-             <Tab.Screen name="Map" component={MapScreen} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="map-outline" size={size} color={color}/>)}}/>
-            <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: ({ color, size}) => (<Ionicons name="settings-outline" size={size} color={color}/>)}}/>
-        </Tab.Navigator>
-    )
+  const { colour } = useTheme();
+
+  return (
+    <Tab.Navigator screenOptions={{headerShown: false, ...createTabBarStyles(colour)}}>
+      <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: HomeTabIcon}}/>
+      <Tab.Screen name="Map" component={MapScreen} options={{tabBarIcon: MapTabIcon}}/>
+      <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: SettingsTabIcon}}/>
+    </Tab.Navigator>
+  );
 }
